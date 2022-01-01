b8c54ed312e8109782f874139e1c04ae04fc8525
yamoto0628 <a18sb009@zr.osaka-cu.ac.jp>
2022-01-01 20:05:17 +0900

    get port fro env

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/yamoto0628/fp_model_sumilation_api/controller"
 	"github.com/yamoto0628/fp_model_sumilation_api/model/repository"
 	"net/http"
+	"os"
 )
 
 var tr = repository.NewFPRepository()
@@ -11,6 +12,10 @@ var tc = controller.NewFPController(tr)
 var ro = controller.NewRouter(tc)
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
 	http.HandleFunc("/api", ro.HandleFPRequest)
-	_ = http.ListenAndServe(":8000", nil)
+	_ = http.ListenAndServe(port, nil)
 }
